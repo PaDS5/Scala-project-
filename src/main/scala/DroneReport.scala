3d6e99fb282b3051f7e2e78c5d04c2792433093c
PaDS5 <paul.dorffer@orange.fr>
2021-03-01 19:55:59 +0100

    Ajout Identity

--- src/main/scala/DroneReport.scala
+++ src/main/scala/DroneReport.scala
@@ -2,12 +2,19 @@ import scala.collection.mutable
 import scala.collection.mutable.HashMap
 
 object DroneReport {
+
+  case class Identity(
+                     firstname: String,
+                     lastname: String,
+                     adress: String,
+                     )
+
   case class Drone(
                     id : Int,
                     lat_location : Double,
                     long_location : Double,
                     words : List[String],
-                    surround : mutable.HashMap[String, Int]
+                    surround : mutable.HashMap[Identity, Int]
   )
 
 }
--- src/main/scala/helloworld.scala
+++ src/main/scala/helloworld.scala
@@ -1,10 +1,15 @@
+import DroneReport.Identity
+
 import scala.collection.mutable
 
 
 object helloworld {
   def main(args: Array[String]): Unit = {
-    println("Hello world")
-    val temp = mutable.HashMap(("1", 50), ("2", 78), ("3", 23))
+
+    val person = Identity("louis", "Dupont", "30 Avenue des Champs-Elysées")
+
+    val temp = mutable.HashMap((person, 50))
+
     val reporttest = DroneReport.Drone(120, 15.4856854, 3.4521684, List("Bien", "Mauvais", "Nul"),temp)
     println("Information du rapport du drone ", reporttest)
   }
